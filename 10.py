1568ed5102a5c29cac1b980b63fa6810d1932669
Adam Kolarik <adam.kolarik@second-foundation.eu>
2021-12-10 22:58:46 +0100

    Improved day10 code
    Drastically improved the code. The principle of the solution is the same, but I used better python code techniques I saw in other solutions.

--- 10.py
+++ 10.py
@@ -1,7 +1,12 @@
 input = open("10-input.txt").read().split()
 
-openingChars = ['(','[','{','<']
-closingChars = [')',']','}','>']
+chunks = {
+  '(':')',
+  '[':']',
+  '{':'}',
+  '<':'>'
+}
+
 score = {
   ')':1,
   ']':2,
@@ -9,36 +14,23 @@ score = {
   '>':4
 }
 
-isOpening = lambda a : a in openingChars
 e=list()
 
 for line in input:
   b=list()
-  kill=False
   for i in line:
-    if isOpening(i):
+    if i in chunks:
       b.append(i)
+    elif (chunks[b[-1]] == i):
+      b.pop()
     else:
-      for j in range(1,len(b)+1):
-        if (closingChars.index(i) == openingChars.index(b[-j])):
-          b.pop(-j)
-          break
-        else:
-          print(f"Expected '{closingChars[openingChars.index(b[-j])]}', but found '{i}' instead")
-          kill=True
-          break
-    if kill:
+      # print(f"Expected '{chunks[b[-1]]}', but found '{i}' instead")
       break
-  if kill:
-    continue
-  # print("unfinished line, counting score")
-  # Complete unfinished line and count score
-  d=0
-  for j in range(1,len(b)+1):
-    # print(f"{closingChars[openingChars.index(b[-j])]}")
-    d = d*5+score[closingChars[openingChars.index(b[-j])]]
-  e.append(d)
+  else:
+    d=0
+    for j in b[::-1]:
+      d = d*5+score[chunks[j]]
+    e.append(d)
 
-print(e)
-e = sorted(e)
+e.sort()
 print(e[len(e)//2])
